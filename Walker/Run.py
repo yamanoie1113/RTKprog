216cc98245ab7f489edcc4492e373ba080839bfa
yamanoie1113 <nkhs.hc39.1047@gmail.com>
2022-09-14 21:47:59 +0900

    Run

--- Walker/Run.py
+++ Walker/Run.py
@@ -1,1 +1,25 @@
-test
+from abc import ABCMeta,abstractmethod
+
+class Run(metaclass=ABCMeta):
+    @abstractmethod
+    
+    
+    def __init__(self,param):
+
+        pass
+
+
+#mPID=new PID()
+#mX=X
+#mY=X
+
+    def set_param(self,X,Y):
+        #mPID=new PID()
+        self.X=X
+        self.Y=Y
+        pass
+
+    def Run():
+
+        pass
+    
