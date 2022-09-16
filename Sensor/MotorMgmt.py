aaa070e2cb41744a8acd669b33aa0fb84121cb5a
hc39-okawa <nkhs.hc39.1007@gmail.com>
2022-09-16 17:51:55 +0900

    Update MotorMgmt.py

--- Sensor/MotorMgmt.py
+++ Sensor/MotorMgmt.py
@@ -1,31 +1,58 @@
-#ifndef WHEEL_MOTOR_MGMT_H_
-#define WHEEL_MOTOR_MGMT_H_
-#include "ev3api.h"
-#include "Motor.h"
-#include "Walker"
-#include "BrightnessSensor.h"
+from time import sleep
+import pigpio
+import time
 
-class WheelMotorMgmt {
-    public:
-        WheelMotorMgmt(ev3api::Motor& leftWheel,
-                                 ev3api::Motor& rightWhee);
-        ~WheelMotorMgmt();
-        void walkDriveOrder(int rpwm,int lpwm);
-        void init();
-        float getRCount();
-        float getLCount();
-           // void armDriveOrder(int pwm);
-       // void tailDriveOrder(int pwm);
-    private:
-        int r_current_encoded_value;
-        int l_current_encoded_value;
-        ev3api::Motor mLeftWheel;
-        ev3api::Motor mRightWheel;
-        //int rpwm;
-       // int lpwm;
-        //int arm_current_encoded_value;
-        //int tail_current_encoded_value;
-};
+class MotorMgmt:
+    def __init__(self):
+        
+        pi = pigpio.pi()
+        pi.set_mode(17, pigpio.OUTPUT)
+        pi.set_mode(22, pigpio.OUTPUT)
+        #PWMパラメータ
+        pwm_pin1 = 18 #PWM出力ピンを指
+        duty = 0 #デューティー比を%で指定
+        #freq = 100 #PWM周波数をHzで指定
+        up_flag = True
+        #IN1、IN2の制御信号
+        pi.write(17, 0)
+        pi.write(22, 1)
 
 
-#endif
+
+
+
+    def set_param(self):
+        pi = pigpio.pi()
+        pi.set_mode(17, pigpio.OUTPUT)
+        pi.set_mode(22, pigpio.OUTPUT)
+        #PWMパラメータ
+        pwm_pin1 = 18 #PWM出力ピンを指
+        duty = 0 #デューティー比を%で指定
+        #freq = 100 #PWM周波数をHzで指定
+        up_flag = True
+        #IN1、IN2の制御信号
+        pi.write(17, 0)
+        pi.write(22, 1)
+        while True:
+    
+        #デューティサイクル計算
+            cnv_dutycycle = int((duty * 1000000 / 100))
+        #PWMを出力
+            #pi.hardware_PWM(pwm_pin1, freq, cnv_dutycycle)
+        #dutyを変更
+            if up_flag == True:
+                if duty == 100:
+                    up_flag = False
+                else:
+                    duty += 1
+            else:
+                if duty == 0:
+                    up_flag = True
+                else:
+                    duty -=1
+    
+            time.sleep(0.05)
+
+            pass
+
+
