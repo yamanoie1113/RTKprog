f928a39efcff25e857abead9ca0c375526185126
hc39-okawa <nkhs.hc39.1007@gmail.com>
2022-09-26 17:46:51 +0900

    Update MotorMgmt.py

--- Sensor/MotorMgmt.py
+++ Sensor/MotorMgmt.py
@@ -3,7 +3,8 @@ import pigpio
 import time
 
 class MotorMgmt:
-    def __init__(self):
+
+    def __init__(freq):
         
         pi = pigpio.pi()
         #PWMパラメータ
@@ -11,26 +12,27 @@ class MotorMgmt:
         pwm_pin2 = 18 #PWM出力ピンを指定
         duty1 = 9#デューティー比を%で指定　スピードモーター
         duty2 = 8#デューティー比を%で指定　サーボモーター
-        freq = 60 #PWM周波数をHzで指定
+        freq = 0 #PWM周波数をHzで指定
+        pwm = float(freq)
         up_flag = True
         #IN1、IN2の制御信号
         cnv_dutycycle1 = int((duty1 * 1000000 / 100))
         cnv_dutycycle2 = int((duty2 * 1000000 / 100))
 
-        pi.hardware_PWM(pwm_pin1, freq, cnv_dutycycle1)
-        pi.hardware_PWM(pwm_pin2, freq, cnv_dutycycle2)
+        pi.hardware_PWM(pwm_pin1, pwm, cnv_dutycycle1)
+        pi.hardware_PWM(pwm_pin2, pwm, cnv_dutycycle2)
 
 
 
 
 
-    def set_param(self):
+    def set_param(freq):
         pi = pigpio.pi()
         #PWMパラメータ
         pwm_pin1 = 19 #PWM出力ピンを指定
         pwm_pin2 = 18 #PWM出力ピンを指定
         duty = 0 #デューティー比を%で指定
-        freq = 60 #PWM周波数をHzで指定
+        pwm = float(freq)
         up_flag = True
         #IN1、IN2の制御信号
         pi.write(17, 0)
@@ -40,25 +42,15 @@ class MotorMgmt:
         #デューティサイクル計算
             cnv_dutycycle = int((duty * 1000000 / 100))
         #PWMを出力
-            #pi.hardware_PWM(pwm_pin1, freq, cnv_dutycycle)
+            pi.hardware_PWM(pwm_pin1, pwm, cnv_dutycycle)
+            pi.hardware_PWM(pwm_pin2, pwm, cnv_dutycycle)
         #dutyを変更
-            if up_flag == True:
-                if duty == 100:
-                    up_flag = False
-                else:
-                    duty += 1
-            else:
-                if duty == 0:
-                    up_flag = True
-                else:
-                    duty -=1
-    
-            time.sleep(0.05)
+           
+
+        
 
 
-            pass
-
-        import pigpio
+            
 
 
 
