077c6fb9c2d50bfa39b0af55c746e3c6a5e08c2d
hc39-okawa <nkhs.hc39.1007@gmail.com>
2022-10-03 21:40:08 +0900

    Create MotorMgmt.py

---
+++ Sensor/MotorMgmt.py
@@ -0,0 +1,37 @@
+from itertools import cycle
+from time import sleep
+import pigpio
+import time
+pi = pigpio.pi()
+
+class MotorMgmt:
+
+        
+    def set_param(a,b,c : float):
+
+        duty = a
+        freq = b
+        cycle = c
+
+
+        return freq,cycle
+
+
+
+    def run():
+        duty,freq,cycle = MotorMgmt.set_param()
+        pin1 = 19 #スピード
+        pin2 = 18 #サーボ
+
+        while True:
+            pi.hardware_PWM(pin1, freq, cycle)
+            pi.hardware_PWM(pin2, freq, cycle)
+
+
+
+
+            
+
+
+
+
