3d9948d90d9e9897595b3205e1c82c503d4d8322
takaken0518 <twiceup0518@gmail.com>
2022-11-16 11:32:27 +0900

    TKPra内でTimeJugdeテスト

---
+++ TKPra/TKTimeJugde.py
@@ -0,0 +1,40 @@
+import sys
+import pathlib
+current_dir = pathlib.Path(__file__).resolve().parent
+sys.path.append(str(current_dir) + '/../')
+
+from TKPra import Timer2
+
+class TKTimejudge():
+
+    time=0.0
+    timelimit = 0.0
+    timer = Timer2.Timer2()
+    def __init__(self):
+        self.set_param(10)
+
+    def judge(self):
+        time = self.timer.getvalue()
+        print("gettime:",end="")
+        print(time)
+
+        if(time >= self.timelimit):
+            return True
+
+        else :
+            return False
+
+
+    def set_param(self,limit):
+        self.timelimit = limit
+        print("limit:",end="")
+        print(limit)
+
+
+def main():
+    test = TKTimejudge()
+    test.judge()
+
+
+if __name__=="__main__":
+     main()
---
+++ TKPra/Timer2.py
@@ -0,0 +1,71 @@
+import time
+import sys
+import pathlib
+import threading
+import random
+
+current_dir = pathlib.Path(__file__).resolve().parent
+sys.path.append(str(current_dir) + '/../')
+from Sensors import Sensor
+
+class Timer2(Sensor.Sensor):
+    #count2 = 0
+    thread1 = None
+    thread2 = None
+
+    def __init__(self):
+        print("init")
+        self.count2=0
+        self.thread1 = threading.Thread(target=self.count)
+        self.thread1.start()
+        self.thread2 = threading.Thread(target=self.getvalue)
+        print("end_init")
+
+
+
+    def update(self):
+        #self.count=0
+        pass
+
+    def count(self):
+        print("count")
+        for j in range(10):#直接数字じゃなくて引数をいれるかも
+            self.start = time.perf_counter()
+            time.sleep(1)
+
+            self.count2+=round(time.perf_counter() - self.start)
+            print(self.count2)
+            #sprint(self.count2)
+            #return self.count2
+
+        print("endcount")
+
+
+
+
+    def getvalue(self):
+
+        print("timer.getvalue")
+        print("return_value:",end="")
+        print(self.count2)
+        return self.count2
+
+
+"""
+def main():
+    timer=Timer2()
+    #timer.update()
+    #timer.set_i()
+    rand = random.randint(0,10)
+    time.sleep(rand)
+    print("rand:",end="")
+    print(rand)
+    timer.thread2.start()
+
+    pass
+
+
+if __name__=="__main__":
+    main()
+"""
+
