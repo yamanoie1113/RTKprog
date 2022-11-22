9cc62515f9eb2c81a284926f3df1d4e8332566cd
hc39-okawa <nkhs.hc39.1007@gmail.com>
2022-11-22 18:47:40 +0900

    Update curveLineTrace.py

--- Walker/curveLineTrace.py
+++ Walker/curveLineTrace.py
@@ -14,61 +14,64 @@ from math import fabs
 from Walker.Run import Run
 
 
+class cuvreLineTrace:
 
 
-class cuvreLineTrace(Run):
-
-     
-    startx = 0
-    starty = 0
-    goalx = 0
-    goaly = 0
-
-
-    def set_param(PositionMgmt,a,b):
+        def set_param(PositionMgmt,a,b):
         
-        PM = PositionMgmt.PositionMgmt()
-        param = PM.PositionMgmt.getvalue()
-        x = param[0] #座標分け
-        y = param[1]
-        a = x-1 #中心点X
-        b = y-1 #中心点Y
-        r = np.sqrt((a-x)**2 + (b-y)**2) #座標計算
-        return r
+            #PM = PositionMgmt.PositionMgmt()
+            #param = PM.PositionMgmt.getvalue()
+            param = [5,7]
+            x = param[0] #座標分け
+            y = param[1]
+            a = x-1 #中心点X
+            b = y-1 #中心点Y
+            r = np.sqrt((a-x)**2 + (b-y)**2) #座標計算
+            return r
 
-    def set_run(self,c):
-
-        PM = PositionMgmt.PositionMgmt()
-        r = 0
-        a = 0 #中心点X
-        b = 0 #中心点Y
-        r = cuvreLineTrace.set_param(0,a,b)
-        loca = r 
-        turn = right
-
-        while True:
-            if r < loca:
-                #中心点に近づく
-                loca = 0
-                if turn == right:
-                    MotorMgmt.set_param(0,1,100)
-                else:
-                    MotorMgmt.set_param(0,30,-100)
-
-            elif r > loca:
-                #中心点から離れる
-                loca = 0
-                if turn == right:
-                    MotorMgmt.set_param(0,30,-100)
-                else:
-                    MotorMgmt.set_param(0,30,100)
+        def set_run(self):
 
             
-            cuvreLineTrace.set_param(PM,a,b)
-            time.sleep(0.1)
-            c += 1
-            if c == 600:
-                break
+            #PM = PositionMgmt.PositionMgmt()
+            r = 0
+            a = 0 #中心点X
+            b = 0 #中心点Y
+            r = cuvreLineTrace.set_param(0,a,b)
+            loca = r 
+            turn = right
+            c = 0
+            
 
-    def main():
-        cuvreLineTrace.set_run(0)
+            while True:
+                if r < loca:
+                    #中心点に近づく
+                    loca = 0
+                    if turn == right:
+                        MotorMgmt.set_param(0,1,100)
+                    else:
+                        MotorMgmt.set_param(0,30,-100)
+
+                elif r > loca:
+                    #中心点から離れる
+                    loca = 0
+                    if turn == right:
+                        MotorMgmt.set_param(0,30,-100)
+                    else:
+                        MotorMgmt.set_param(0,30,100)
+
+            
+                cuvreLineTrace.set_param(PM,a,b)
+                time.sleep(0.1)
+                c += 1
+                if c == 600:
+                    break
+                print (c)
+                
+
+def main():
+    print (1)
+    cuvre = cuvreLineTrace()
+    cuvre.set_run()
+        
+if __name__ == '__main__':
+    main()
