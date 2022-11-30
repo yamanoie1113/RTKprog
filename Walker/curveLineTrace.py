864759d6fe3b6b829b063647a679a316938c190d
hc39-okawa <nkhs.hc39.1007@gmail.com>
2022-11-30 16:01:35 +0900

    Update curveLineTrace.py

--- Walker/curveLineTrace.py
+++ Walker/curveLineTrace.py
@@ -8,7 +8,7 @@ current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append(str(current_dir) + '/../')
 from Sensors import MotorMgmt
 from Sensors import PositionMgmt
-#from section import SectionRun2
+from section import SectionRun
 from tkinter import W
 from turtle import right
 import numpy as np
@@ -19,27 +19,27 @@ class cuvreLineTrace:
 
         def set_param(a,b):
         
-            PM = PositionMgmt.PositionMgmt()
-            param = PM.getvalue()
-            x = param[0] #座標分け
-            y = param[1]
-            a = x-300 #中心点X
-            b = y-300 #中心点Y
+            #PM = PositionMgmt.PositionMgmt()
+            #para = PM.getvalue()
+            para = [500,500]
+            x = para[0] #座標分け
+            y = para[1]
+            #a = x-300 #中心点X
+            #b = y-300 #中心点Y
 
             r = np.sqrt((a-x)**2 + (b-y)**2) #座標計算
             return r
 
-        def set_run(self):
+        def set_run(self,p,v):
 
             #self.mPID=PID()
             #self.mPID.reset_param()
             #self.param = list()
-            #self.param = SectionRun2.set_param()
             MM = MotorMgmt.MotorMgmt()
             r = 0 #現在半径
             loca = 0 #目標半径
-            a = 0 #中心点X
-            b = 0 #中心点Y
+            a = 300 #中心点X
+            b = 300 #中心点Y
             r = cuvreLineTrace.set_param(a,b)
             loca = r
             turn = right #旋回半径
@@ -51,31 +51,38 @@ class cuvreLineTrace:
                 #self.mPID.set_target(loca)
                 #self.mPID.set_Kpid(self.param[2],self.param[3],self.param[4])
                 #self.mPID.get_operation()
+
                 
                 if r < loca:
                     #中心点に近づく
                     if turn == right:
-                        MM.set_param(1,100)
+                        MM.set_param(p,v)
+                        #MM.set_param(1,100)
                         #print ('zennsin')
                     else:
                         #print ('cousin')
-                        MM.set_param(10,-100)
+                        MM.set_param(p,v)
+                        #MM.set_param(10,-100)
 
                 elif r > loca:
                     #中心点から離れる
                     if turn == right:
                         #print ('zennsin2')
-                        MM.set_param(1,-100)
+                        MM.set_param(p,v)
+                        #MM.set_param(1,-100)
                     else:
-                        MM.set_param(10,100)
+                        #MM.set_param(10,100)
+                        MM.set_param(p,v)
                         #print ('cousin2')
                 else:
                     if turn == right:
-                        MM.set_param(1,100)
+                        #MM.set_param(1,100)
+                        MM.set_param(p,v)
                         #print ('zennsin')
                     else:
                         #print ('cousin')
-                        MM.set_param(10,-100)
+                        MM.set_param(p,v)
+                        #MM.set_param(10,-100)
 
                 MM.run()
                 r = cuvreLineTrace.set_param(a,b)
@@ -92,7 +99,7 @@ class cuvreLineTrace:
 
 def main():
     #print (1)
-    cuvre = cuvreLineTrace()
+    cuvre = cuvreLineTrace(1,100)
     cuvre.set_run()
         
 if __name__ == '__main__':
