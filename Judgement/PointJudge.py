9ff4933903016d7c79e94de9f9d25d0b1288c258
takaken0518 <twiceup0518@gmail.com>
2023-02-22 14:15:55 +0900

    po

--- Judgement/PointJudge.py
+++ Judgement/PointJudge.py
@@ -10,8 +10,12 @@ from Judgement import Judge
 from Sensors import PositionMgmt as PMgmt,TurnAngleSensor as TASensor
 
 class PointJudge(Judge.Judge):
+
+    start_x=0.0    #X座標系　オブジェクト
+    start_y=0.0   #Y座標系　オブジェクト
+
     def __init__(self):
-        pass
+        self.pget = PMgmt.PositionMgmt()
 
     def judge():
         if True:
@@ -23,3 +27,10 @@ class PointJudge(Judge.Judge):
     def set_param():
 
         pass
+
+    def getPosition(self):
+        positionXY = self.pget.getvalue()
+
+        #mx ,myに座標をセット
+        self.start_x = positionXY[0]
+        self.start_y = positionXY[1]
