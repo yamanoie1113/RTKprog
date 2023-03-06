3846fd54e12e435d2299dc83538a1bc3e0c90321
hc39-okawa <nkhs.hc39.1007@gmail.com>
2023-03-06 17:48:21 +0900

    0

---
+++ cmt.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+from multiprocessing import Process
+import os
+import sys
+import pathlib
+current_dir = pathlib.Path(__file__).resolve().parent
+sys.path.append(str(current_dir) + '/../')
+import rpipwmtest
+#import pigpio
+
+
+
+def main():
+    print("b")
+    rpip = rpipwmtest.rpipwmtest()
+    rpip.callb()
+    print("v")
+    
+def ant():
+    print("d")
+    return 0
+        
+if __name__ == '__main__':
+    main()
--- rpipwmtest.py
+++ rpipwmtest.py
@@ -1,4 +1,5 @@
 import pigpio
+import cmt
 from time import sleep
 
 ch1_rise_tick = 0
@@ -9,18 +10,37 @@ ch2_in = 24
 
 ch1_out = 18
 ch2_out = 19
-on = True
+on = 0
 pi = pigpio.pi()
 
 class rpipwmtest:
+    
+    def callb(self):
+        
+        global ch1_in, ch2_in
+        global ch1_out, ch2_out
+        global on
+        print("s")
+        pi.set_mode(ch1_out, pigpio.OUTPUT)
+        pi.set_mode(ch2_out, pigpio.OUTPUT)
+        pi.set_mode(ch1_in, pigpio.INPUT)
+        pi.set_mode(ch2_in, pigpio.INPUT)
+        rp = pi.callback(ch1_in, pigpio.EITHER_EDGE, rpipwmtest.gpiocallback)
+        rp = pi.callback(ch2_in, pigpio.EITHER_EDGE, rpipwmtest.gpiocallback)
+        while True:
+            sleep(1)
+            
     
     def gpiocallback(gpio, level, tick):
         global ch1_rise_tick, ch2_rise_tick
         global ch1_in, ch2_in
         global ch1_out, ch2_out
         global on
-        on = False
+        mgmt = 0
         if gpio == ch1_in :
+            if on == 0 :
+                rpipwmtest.mgmt(0)
+                on = 1
             if level==1 :
                 ch1_rise_tick = tick
             elif level==0 : 
@@ -39,27 +59,12 @@ class rpipwmtest:
                     pi.set_servo_pulsewidth(ch2_out, pwm_width)
             else :
                 pi.set_servo_pulsewidth(ch2_out, 0)
-                
-        
+                 
                     
-    def callb(self):
-        
-        global ch1_in, ch2_in
-        global ch1_out, ch2_out
-        global on
-        power = 0
-        print("s")
-        pi.set_mode(ch1_out, pigpio.OUTPUT)
-        pi.set_mode(ch2_out, pigpio.OUTPUT)
-        pi.set_mode(ch1_in, pigpio.INPUT)
-        pi.set_mode(ch2_in, pigpio.INPUT)
-        rp = pi.callback(ch1_in, pigpio.EITHER_EDGE, rpipwmtest.gpiocallback)
-        rp = pi.callback(ch2_in, pigpio.EITHER_EDGE, rpipwmtest.gpiocallback)
-        while True:
-            if power == 0 and on == False:
-                power = 1
-                return on
-            sleep(1)
+    def mgmt(self):
+        print("a")
+        cmt.ant()
+        return 0
             
             
 def main():
@@ -68,3 +73,4 @@ def main():
         
 if __name__ == '__main__':
     main()
+
