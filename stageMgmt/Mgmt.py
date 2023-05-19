d17b956c1b3e08c4a2d79073bafc29963838bea1
yamanoie1113 <85339458+yamanoie1113@users.noreply.github.com>
2023-05-19 14:15:09 +0900

    stageMgmt修正、フォルダ名変更

--- stageMgmt/Mgmt.py
+++ stageMgmt/Mgmt.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process,Value
+#from multiprocessing import Process
 #from concurrent.futures import ProcessPoolExecutor
 #import multiprocessing
 import sys
@@ -7,10 +7,9 @@ current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append(str(current_dir) + '/../')
 #from yamagapractice import Test
 from section import SectionMgmt
-from section import b
-from Sensors import MotorMgmt
+
 #import Timerset
-import rpipwmtest
+#import rpipwmtest
 import time
 
 # メインプロセスで動かす関数
@@ -18,12 +17,10 @@ import time
 class Mgmt:
 
     #signal=True
-    motormgmt=MotorMgmt.MotorMgmt()
 
     def __init__(self):
     
         print("プログラム開始")
-        self.count = Value('i', True) #初期値１
     
     def run(self):
         print('メインプロセスStart')
@@ -35,34 +32,20 @@ class Mgmt:
         counter=0
         state=True #ステートの設定
 
-        if self.count.value==False:
-            self.end()
+        section.run()
 
-        else:
-
-            section.run()#sectionの準備
-
-            while state:
+        while state:
                 
-                state2=self.timejudge1(start_time)  #時間測定
+            state2=self.timejudge1(start_time)
 
-                if self.count.value==False:
-                    state=False
-                    break
-
-                elif (state2>=100): #走行時間の設定 プロボの電源が入ると終了
-                    state=False
-                    #self.motormgmt.run(0,0)
-                    break
-            
-                else:
-                    counter+=1
-                    print('tesuto')
-                    section.execRun()#section実行
-                    print(counter,"回目")
-        
-
-        #self.GPS()
+            if (state2>=10): #走行時間の設定
+                state=False
+                break
+                
+            counter+=1
+            print('tesuto')
+            section.execRun()
+            print(counter,"回目")
 
         print("終了")
 
@@ -75,56 +58,29 @@ class Mgmt:
             print("経過時間",end_time)
             return end_time
         
-    def end(self):
-        print("all end")
+
+    def test(self):
+        section=SectionMgmt.SectionMgmt()
+        section.run()
+        section.test()
+
+
         
 
 
-'''
-    # 緊急停止
-    def motor_stop(self):
-        print('緊急停止待機状態Start')
-        #statement=None
-        Motor=Test.Test()
-        #Motor=rpipwmtest.rpipwmtest()
-        self.count.value=Motor.Re()
-        #self.count.value=Motor.callb()
-        print(self.count.value) #valueを使用することで変数を共有することができる
-
+def main():
         
-        
-        for i in range(101):
-            time.sleep(1)
-            print(f'func_2 {i}')
-        print('サブプロセス2End')
-        
+    mgmt=Mgmt()
+    #mgmt.test()
+    mgmt.run()
 
-    #GPS起動
-    def GPS(self):
-        print("GPS_Start")
-        for i in range(10):
-            time.sleep(1)
-            print(f'func_3 {i}')
-        print('GPS_End')
-    
-    '''
+        #p2 = Process(target=self.motor_stop)
+        #p3 = Process(target=self.GPS)
 
-
-def main(self):
-        
-    p1 = Process(target=self.motor_stop)
-    p2 = Process(target=self.run)
-    #p3 = Process(target=self.GPS)
-
-        
-    p1.start()
-    time.sleep(20)
-    p2.start()
-    #p3.start()
-
+        #p1.start()
+        #p2.start()
+        #p3.start()
 
 if __name__ == '__main__':
-    #main()
+    main()
 
-    m=Mgmt()
-    m.main()
