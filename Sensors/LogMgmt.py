d81236b502ca2f4ddff7fc43e783883deaa5bf53
takaken0518 <twiceup0518@gmail.com>
2023-06-22 10:37:18 +0900

    ログをLOGに書きこむように変更

--- Sensors/LogMgmt.py
+++ Sensors/LogMgmt.py
@@ -1,10 +1,16 @@
 import datetime
+import sys
+import pathlib
+
+dir = pathlib.Path(__file__).resolve().parent
+dir = (str(dir) + '/../LOG')
+
 #ログの内容を消去する
-def clear(path):
-    f = open(path,"w")
+def clear(filename):
+    f = open(dir + filename,"w")
     f.close()
 
-def write(path,position):
-    f = open(path,"a")
+def write(filename,param):
+    f = open(dir + filename,"a")
     f.write(str(datetime.datetime.now()) + ':')
-    f.write(str(position) + ',\n')
+    f.write(str(param) + ',\n')
