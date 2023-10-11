32bcba7ee0d0254a3c3d8eae87c91e3605e348f8
hc39-okawa <nkhs.hc39.1007@gmail.com>
2023-10-11 14:45:59 +0900

    1

--- Walker/VirtualLineTrace.py
+++ Walker/VirtualLineTrace.py
@@ -73,14 +73,15 @@ class VirtualLineTrace():
             #x3 = y - self.starty - slope * (-1 * (self.startx)) / slpoe 
             distance = abs(slope * (x) - (y) + intercept) / math.sqrt(slope**2 + 1)
             LogMgmt.write(self.logfile,distance)
-            
+            #nx = (y-intercept)/slope
+            #最短座標
             t = (-1*slope * x - (-1) * y - intercept) / (slope * slope + (-1) * (-1))
             nx = x + slope * t
             ny = y + (-1) * t
             #tyokusen = np.sqrt((self.goalx - x)**2+(self.goaly - y)**2)
             #print(tyokusen)
             #r = abs(slope * (x) + 1 * y) / np.sqrt(slope**2 + 1**2) #直線との最短距離
-            VirtualLineTrace.set_turn(self,distance,x,y,nx,ny)
+            VirtualLineTrace.set_turn(self,distance,x,y,nx)
             #print(self.goalx,self.goaly)
             #print(x,y)
 
@@ -142,6 +143,7 @@ class VirtualLineTrace():
                     else:
                         self.turn = 'left'
                         self.save_saitan = self.save_saitan * (-1)
+            print(self.turn)
             
 
             
