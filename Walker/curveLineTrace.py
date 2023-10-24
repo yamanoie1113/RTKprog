4d9d13018d699b0de9419d74d65eac3b3b0a4833
hc39-okawa <nkhs.hc39.1007@gmail.com>
2023-10-24 11:22:36 +0900

    Update curveLineTrace.py

--- Walker/curveLineTrace.py
+++ Walker/curveLineTrace.py
@@ -46,17 +46,11 @@ class cuvreLineTrace:
         def __init__(self):
             
             pass
-            # クラス変数
-            #self.logfile = 'cuvreLine_log.txt'
-            #GPSログの消去
-            #LogMgmt.clear(self.logfile)
-            #LogMgmt.write(self.logfile,"NONE_DISTANCE")
+            
         
         def set_first(self):
             #print("test_value",self.test)
             #print(self.param)
-            #x = float(self.param[0])
-            #y = float(self.param[1])
             #print("x,y",x,y)
             #print(self.goaly)            
             self.tyusinx = (self.startx + self.goalx)/2
@@ -64,8 +58,6 @@ class cuvreLineTrace:
             print(self.tyusinx,"  ",self.tyusiny)
             self.standard = (np.sqrt((self.tyusinx-self.goalx)**2 + (self.tyusiny-self.goaly)**2))
             #print("standard",self.standard)
-            #LogMgmt.write(self.logfile,self.distance)
-            #r = abs(slope * (x) + 1 * y) / np.sqrt(slope**2 + 1**2) #直線との最短距離
             #print(self.goalx,self.goaly)
             #print(x,y)
         
@@ -80,27 +72,9 @@ class cuvreLineTrace:
             self.distance = (np.sqrt((self.tyusinx-x)**2 + (self.tyusiny-y)**2)) 
             #print("基準",self.standard)
             #print("現在",self.distance)
-            #LogMgmt.write(self.logfile,self.distance)
-            #r = abs(slope * (x) + 1 * y) / np.sqrt(slope**2 + 1**2) #直線との最短距離
-            #cuvreLineTrace.set_turn(self,self.distance)
             #print(self.goalx,self.goaly)
             #print(x,y)
             #return self.distance
-
-        def set_turn(self,distance):
-            #distance = self.saitan - distance
-            if self.save_turn == 'right':
-                if self.standard < distance :
-                    self.turn = 'right'
-                else:
-                    self.turn = 'left'
-            elif self.save_turn == 'left':
-                if self.standard < distance :
-                    self.turn = 'left'
-                else:
-                    self.turn = 'right'
-            self.save_turn = self.turn
-            #VirtualLineTrace.set_saitan(self,distance)
 
         def set_param(self):
         
@@ -109,51 +83,17 @@ class cuvreLineTrace:
             #self.param[a][0] = 500
             #self.param[a][1] = 500
             #return self.param
-
-        def PID(self,kp,ki,kd,theta_goal,theta_current,error_sum,error_pre):
-            
-            #pidが小さい時
-            '''
-            if theta_current < 1 and theta_current > 0:
-                theta_current = theta_current *100
-            elif theta_current > -1 and theta_current < 0:
-                theta_current = theta_current *10
-            '''
-            theta_current = theta_current*10
-            error = theta_goal - (theta_current)# 偏差（error）を計算            
-            error_sum += error*0.01 # 偏差の総和（積分）を計算
-            #ki = 0       		
-            error_diff = (error-error_pre)/0.5 # PI制御からの追加：1時刻前の偏差と現在の偏差の差分（微分）を計算    		
-            m = (kp * error) + (ki * error_sum) + (kd * error_diff) # 操作量を計算             
-            m = m
-            #print("m",m)
-            m = math.floor(m)
-            if m >= 100:
-                m = 90
-            elif m <= -100:
-                m = -90
-            #print("m",m)
-            return m, error_sum, error
         
         def init_state(self):
             self.cancel = 0
         
         def set_run(self,paramlist,goaly,Positionmgmt):
-            #self.mPID=PID()
-            #self.mPID.reset_param()
-            #self.param = list()
-            #param_list = [0]*5
-            #for f in range(4):
-                #param_list = paramlist[f:f+1]
-            #self.PM.__init__(self)
             
             self.sp = paramlist[0]
             self.sv = 0
             self.p = paramlist[1]
             self.i = paramlist[2]
             self.d = paramlist[3]
-            #self.goalx = goaly[0]
-            #self.goaly = goaly[1]
             #print(self.goalx)
             #print("curve_goal")
             #print(goaly)
