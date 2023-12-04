7601f27580ed856a7871947576327708ca242a9a
hc39-okawa <nkhs.hc39.1007@gmail.com>
2023-12-04 09:37:38 +0900

    Update VirtualLineTrace.py

--- Walker/VirtualLineTrace.py
+++ Walker/VirtualLineTrace.py
@@ -24,6 +24,8 @@ class VirtualLineTrace():
         save_turn = 'no'
         saitan = 0
         save_saitan = 0
+        slope = 0
+        intercept = 0
         #save_saitan = 0
         MM = MotorMgmt.MotorMgmt()
         PM = None
@@ -61,11 +63,7 @@ class VirtualLineTrace():
             
             #print(x,y)
             #print(self.goaly)
-            #print('distance')
-            
-            slope = (self.goaly - self.starty)/(self.goalx - self.startx)
-            #print(slope)
-            intercept = self.starty - slope * self.startx
+            #print('distance')            
             '''if slope == 0:
                 x3 = x
             else:
@@ -73,14 +71,14 @@ class VirtualLineTrace():
             '''
             
             #x3 = y - self.starty - slope * (-1 * (self.startx)) / slpoe 
-            distance = abs(slope * (x) - (y) + intercept) / math.sqrt(slope**2 + 1)
+            distance = abs(self.slope * (x) - (y) + self.intercept) / math.sqrt(self.slope**2 + 1)
             #print(distance)
             #LogMgmt.write(self.logfile,distance)
             #nx = (y-intercept)/slope
             #最短座標
             #'''
-            t = (-1*slope * x - (-1) * y - intercept) / (slope * slope + (-1) * (-1))
-            nx = x + slope * t
+            t = (-1*self.slope * x - (-1) * y - self.intercept) / (self.slope * self.slope + (-1) * (-1))
+            nx = x + self.slope * t
             ny = y + (-1) * t
             #'''
             #tyokusen = np.sqrt((self.goalx - x)**2+(self.goaly - y)**2)
@@ -101,7 +99,21 @@ class VirtualLineTrace():
                 self.bunp = 4
 
         def set_turn(self,distance,x,y,nx,ny):
-            
+            #ONOFF回路の
+            ''' 
+            if 0 <= distance:
+                #distance = self.saitan - distance
+                self.turn = 'right'
+                self.save_saitan = distance
+                if self.saitan < distance and self.save_turn == 'right':
+                    self.turn = 'left'
+                    self.save_saitan = self.save_saitan * (-1)
+            else:
+                self.turn = 'no'
+            self.save_turn = self.turn
+            self.saitan = distance
+            #VirtualLineTrace.set_saitan(self,distance)
+            '''
             if 0 <= distance:
                 #print('distance',distance)
                 #print('self.save_saitan',self.save_saitan)
@@ -115,22 +127,16 @@ class VirtualLineTrace():
                 if self.bunp ==3 or self.bunp ==4:
                     if nx <= x:
                         self.turn = 'right'
-                        
                     else:
                         self.turn = 'left'
                         self.save_saitan = self.save_saitan * (-1)
-                        
             #print(self.turn)
             #'''
 
         def set_param(self):
-        
-            #PM = PositionMgmt.PositionMgmt()
+
             #print("kite")
             self.param = self.PM.getvalue()
-            #self.param[a][0] = 500
-            #self.param[a][1] = 500
-            #return self.param
 
 
         def fast_param(self,a,b,c):
@@ -178,27 +184,15 @@ class VirtualLineTrace():
 
         def set_run(self,paramlist,goaly,Positionmgmt):
             #print("dete")
-            #self.mPID=PID()
-            #self.mPID.reset_param()
-            #self.param = list()
-            #param_list = [0]*5
-            #for f in range(4):
-                #param_list = paramlist[f:f+1]
-            #self.PM.__init__(self)
             self.PM=Positionmgmt
             self.sp = paramlist[0]
             self.sv = 0
             self.p = paramlist[1]
             self.i = paramlist[2]
             self.d = paramlist[3]
-            #self.goalx = goaly[0]
-            #self.goaly = goaly[1]
-            #print(self.goalx)
             VirtualLineTrace.set_param(self)
             #print("param")
             #print(self.param)            
-            
-            #
             #print(self.goalx,self.goaly)
             #print(self.startx,self.starty)
             #ループカウンタ
@@ -207,11 +201,11 @@ class VirtualLineTrace():
                 self.goaly = float(goaly[1])
                 self.startx = float(self.param[0])
                 self.starty = float(self.param[1])
-                VirtualLineTrace.set_bunp(self)
-                print("byup:",self.bunp)
-                print("goalx,y",self.goalx,self.goaly)
                 self.error_sum = 0
                 self.error_pre = 0
+                VirtualLineTrace.set_bunp(self)
+                self.slope = (self.goaly - self.starty)/(self.goalx - self.startx)
+                self.intercept = self.starty - self.slope * self.startx
                 self.cancel = 1                
             self.run()            
 
@@ -279,4 +273,4 @@ if __name__ == '__main__':
 
 
 
-                                                                
+
