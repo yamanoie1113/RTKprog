7343111735a66093e6b958429c3b23e8682ba2b8
takaken0518 <twiceup0518@gmail.com>
2024-01-26 10:26:18 +0900

    a

--- TKPractice/midtest.py
+++
@@ -1,3 +0,0 @@
-x = int(input())
-x2 = int(input())
-print(str((x + x2) /2))
--- TKPractice/stdout.py
+++
@@ -1,9 +0,0 @@
-import sys
-import time
-for i in range(10):
-
-    sys.stdout.write("\r{}".format(i))
-    sys.stdout.flush()
-    time.sleep(0.1)
-    
-print()
